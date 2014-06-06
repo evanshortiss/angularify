a8d63a07e24a66d746f3b88cb248588e8c707df0
Evan Shortiss <evan.shortiss@feedhenry.com>
2014-06-07 00:39:32 +0100

    Update with code checks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,70 @@
 'use strict';
 
 module.exports = function(grunt) {
-  require('grunt-loader').load(grunt);
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    column_lint: {
+      files: {
+        src: ['./lib/**/*.js']
+      },
+    },
+
+    jshint: {
+      src: ['./lib/**/*.js'],
+      options: {
+        globals: {},
+        curly: true,
+        camelcase: false,
+        evil: false,
+        browser: true,
+        trailing: true,
+        sub: true,
+        eqeqeq: false,
+        eqnull: true,
+        devel: false,
+        smarttabs: false,
+        laxbreak: false,
+        laxcomma: true,
+        jquery: false,
+        loopfunc: true,
+        indent: 2,
+        bitwise: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        undef: true,
+        boss: true,
+        node: true,
+        newcap: true,
+        quotmark: 'single',
+        unused: true,
+        strict: true,
+        maxparams: 5,
+        maxdepth: 5,
+        maxstatements: 20,
+        maxcomplexity: 10
+      }
+    },
+
+    lintspaces: {
+      javascript: {
+        src: ['./lib/**/*.js'],
+        options: {
+          indentation: 'spaces',
+          spaces: 2,
+          newline: true,
+          trailingspaces: true,
+          ignores: ['js-comments']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-lintspaces');
+  grunt.loadNpmTasks('grunt-column-lint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('format', ['lintspaces', 'jshint', 'column_lint']);
 };
--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 angular-browserify
 ==================
 
-Load angular dependencies in a browserify friendly manner with simple version selection.
+Browserify transform to load angular dependencies in a browserify friendly 
+manner with simple version selection via the _require_ statement.
+
+## Install (NOT YET PUBLISHED TO NPM)
+```
+npm install angularify
+```
+
+## Usage in Code
+In your application entry file simply use the standard _require_ statement to 
+have a speecific version of Angular or a code Angular module loaded. You only 
+need to do this once as Angular is then made global and modules will be bound 
+in the usual Angular manner! 
+
+An example Browserify entry point file is below.
+
+```javascript
+// Angular is now included in your browserify build (bundle.js)
+require('angular@1.2.2');
+
+// ngRoute is now included in your build too!
+require('angular-route@1.2.2');
+
+angular.module('MyApp', ['ngRoute']);
+
+// Now write your app!
+```
+
+## TODO
+Tests. I have just made this repository public and gotten the transform 
+behaving, but now it needs to be tested to verify it works the way one expects 
+it to in the browser.
